28eb3d50c71431fc4efd4077490c590b2706878b
wnahswl <wnahswl@naver.com>
2024-01-22 11:22:42 +0900

    properties remove

--- src/main/java/edu/pnu/domain/Role.java
+++
@@ -1,6 +0,0 @@
-package edu.pnu.domain;
-
-public enum Role {
-	ROLE_USER, ROLE_ADMIN
-
-}
--- src/main/java/edu/pnu/exception/MemberNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package edu.pnu.exception;
-
-public class MemberNotFoundException extends RuntimeException{
- public MemberNotFoundException(String message) {
-        super(message);
-    }
-}
-
